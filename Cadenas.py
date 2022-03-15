d14f0650f74ad78f769253ff588347248bec3096
Natalia Castro Franco <nataliacastrofranco@MacBook-Air-de-Natalia.local>
2022-03-14 21:18:50 -0600

    POO

---
+++ Cadenas.py
@@ -0,0 +1,9 @@
+#Cadenas y ejemplos
+mensaje1 = str.capitalize('Esto es una PRUEBA de capitalize')
+print(mensaje1)
+
+mensaje2 = 'otra Prueba de capitalize'.capitalize()
+print(mensaje2)
+
+mensaje3 = 'Tercera y última de capitalize'
+print(mensaje3.capitalize())
